66352c6237d1541a413489f493b42c95db57c19b
Miguel Nieto <migustanieto.2@gmail.com>
2021-08-11 15:59:33 -0500

    Introduccion JS

---
+++ Clase-11-08-2021/code.js
@@ -0,0 +1,34 @@
+let dia_hoy = "mar"
+
+if (dia === "lunes") {
+    console.warn('Es lunes... el descanso se acabo')
+} else if (dia == "martes") {
+    console.warn('Es martes... todavia no soy feliz')
+} else if (dia == "miercoles") {
+    console.warn("Ya casi se acaba la semana")
+} else if (dia == "jueves") {
+    console.warn('Ya casi se acaba la semana :D')
+} else if (dia == 'viernes') {
+    console.warn("Es viernes y el cuerpo lo sabe")
+} else {
+    console.error("NO se trabaja, descanse :D")
+}
+
+
+// let numero = 3
+
+// if (numero != 3) {
+//     console.log("cae en el if")
+// } else {
+//     console.error("cae en el else")
+// }
+
+
+// let hay_clase = true
+// let cant_estudiantes = 3
+
+// if (hay_clase && cant_estudiantes <= 3) {
+//     console.warn("Si hay clase y el numero de estudianes es correcto")
+// } else {
+//     console.error('No hay clase')
+// }
