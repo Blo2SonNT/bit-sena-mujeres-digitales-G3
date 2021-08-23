a039463dd056699bfe183015b63c286acf86805e
Miguel Nieto <migustanieto.2@gmail.com>
2021-08-23 16:25:17 -0500

    Ejemplo de pagina con Bootstrap

--- Clase-20-08-2021/js/code_gatito.js
+++
@@ -1,8 +0,0 @@
-function saludo() {
-    Swal.fire({
-        icon: 'warning',
-        title: 'Hola',
-        text: 'Como estas?',
-        confirmButtonText: "Hola",
-    })
-}
---
+++ Clase-23-08-2021/js/config-page.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $('.slider-p').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        arrows: true,
+        infinite: true
+    });
+});
