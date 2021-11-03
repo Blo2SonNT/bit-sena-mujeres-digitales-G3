862b2c45548f25d8ad0d213c88418d7c90e38545
Miguel Nieto <migustanieto.2@gmail.com>
2021-11-03 15:44:34 -0500

    UPDATE y CREATE - Integracion angular express

--- Clase-05-10-2021-ProyectoAngularAPI/sistematienda/src/app/components/crear-producto/crear-producto.component.ts
+++ Clase-05-10-2021-ProyectoAngularAPI/sistematienda/src/app/components/crear-producto/crear-producto.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import Swal from 'sweetalert2';
 import { Producto } from 'src/app/models/producto';
+import { ProductoService } from 'src/app/services/producto.service';
 
 @Component({
 	selector: 'app-crear-producto',
@@ -12,38 +13,77 @@ import { Producto } from 'src/app/models/producto';
 export class CrearProductoComponent implements OnInit {
 
 	productoForm: FormGroup;
+	titulo_formulario = "Crear producto";
+	id: String | null;
 
-	constructor(private fb: FormBuilder, private router: Router) {
+	constructor(private fb: FormBuilder, private router: Router, private _productoService: ProductoService, private idRoute: ActivatedRoute) {
 		this.productoForm = this.fb.group({
-			productoNombre : ['', [Validators.required]], //, Validators.email, Validators.minLength(5)
-			productoPasillo : ['', Validators.required],
+			productoNombre: ['', [Validators.required]], //, Validators.email, Validators.minLength(5)
+			productoPasillo: ['', Validators.required],
 			productoValor: ['', Validators.required],
 			productoProveedor: ['', Validators.required]
 		})
+		this.id = this.idRoute.snapshot.paramMap.get('id');
 	}
 
 	ngOnInit(): void {
+		this.accionSolicitada();
 	}
 
-	agregarProducto(){
+	manipulacion_data_producto() {
 		console.log(this.productoForm);
 
 		// console.log(this.productoForm.get('productoNombre')?.value);
 
 		const PRODUCTO: Producto = {
-			nombre: this.productoForm.get('productoNombre')?.value, 
-			ubicacion: this.productoForm.get('productoPasillo')?.value, 
-			precio: this.productoForm.get('productoValor')?.value, 
+			nombre: this.productoForm.get('productoNombre')?.value,
+			ubicacion: this.productoForm.get('productoPasillo')?.value,
+			precio: this.productoForm.get('productoValor')?.value,
 			proveedor: this.productoForm.get('productoProveedor')?.value
 		}
 
 		console.log(PRODUCTO)
 
-		this.router.navigate(['/']);
-		Swal.fire({
-			icon: 'success',
-			title: 'Producto registrado!'
-		})
+		if(this.id !== null){
+			//editamos producto
+			this._productoService.putProducto(this.id, PRODUCTO).subscribe(data => {
+				this.router.navigate(['/']);
+				Swal.fire({
+					icon: 'success',
+					title: 'Producto ha si actualizado!'
+				})
+			}, error => {
+				console.log(error)
+			})
+		}else{
+			//creamos producto
+			this._productoService.postProductos(PRODUCTO).subscribe(data => {
+				this.router.navigate(['/']);
+				Swal.fire({
+					icon: 'success',
+					title: 'Producto registrado!'
+				})
+			}, error => {
+				console.log(error)
+			})
+		}
+
+	}
+
+
+	accionSolicitada() {
+		if (this.id !== null) {
+			//editamos producto
+			this.titulo_formulario = "Editar producto";
+			this._productoService.getProducto(this.id).subscribe(data => {
+				this.productoForm.setValue({
+					productoNombre: data.nombre, //, Validators.email, Validators.minLength(5)
+					productoPasillo: data.ubicacion,
+					productoValor: data.precio,
+					productoProveedor: data.proveedor
+				})
+			})
+		} 
 	}
 
 }
--- Clase-05-10-2021-ProyectoAngularAPI/sistematienda/src/app/services/producto.service.ts
+++ Clase-05-10-2021-ProyectoAngularAPI/sistematienda/src/app/services/producto.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Producto } from '../models/producto';
 
 @Injectable({
 	providedIn: 'root'
@@ -18,5 +19,17 @@ export class ProductoService {
 
 	deleteProducto(id: String): Observable<any>{
 		return this.http.delete(this.url + id);
+	}
+
+	postProductos(producto: Producto): Observable<any>{
+		return this.http.post(this.url, producto)
+	}
+
+	getProducto(id: String): Observable<any>{
+		return this.http.get(this.url + id);
+	}
+
+	putProducto(id: String, producto: Producto): Observable<any>{
+		return this.http.put(this.url + id, producto);
 	}
 }
